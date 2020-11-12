75df3f1368145fbae0f5630b44317bc7da0c6248
Denise Kruse <deniselkruse@gmail.com>
2020-11-11 20:36:47 -0500

    denise

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
 import './App.css';
+import React from 'react'
+
+import Sitebar from './components/home/Navbar'
+import Header from './components/home/Header'
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <p>This app is a Beast!</p>
+      <Header/>
+      <Sitebar/>
     </div>
   );
 }
+    
+      
 
 export default App;
